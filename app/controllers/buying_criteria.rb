8a5752744972ea5724a8d33438d233df57ac01f5
Alison Ostendorf <aostendorf@gmail.com>
2017-03-22 20:54:56 -0400

    try to add some of buying_criteria views but routes is no working

--- app/controllers/buying_criteria.rb
+++ app/controllers/buying_criteria.rb
@@ -1,17 +1,31 @@
 class BuyingCriteriaController < ApplicationController
+	before_action :find_buying_criteria, only: [:edit, :udpate, :destroy]
+
 
 	def index
-		@buying_criteria = BuyingCriterium.all
+		@buying_criteria = BuyingCriteria.all
+		@property_matches
 	end
 
 	def show
+		@buying_criteria(buying_criteria_params)
+		@property_match
+		# make sure that matches are based on current_user only
 	end
 
 	def new
-		@buying_criteria = BuyingCriterium.new
+		@buying_criteria = BuyingCriteria.all
+		@new_buying_criterium = BuyingCriterium.new
 	end
 
 	def create
+		if
+			@buying_criteria.save
+			redirect_to @buying_criteria
+				# redirect to show page for that buying criteria and its matches
+		else
+			redirect_to new_buying_criterium_path
+		end
 	end
 
 	def edit
@@ -25,6 +39,7 @@ class BuyingCriteriaController < ApplicationController
 
 private
 	def buying_criteria_params
+
 	end
 
 	def find_buying_criteria
